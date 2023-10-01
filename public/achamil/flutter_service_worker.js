4820f554c211db1d1e97f5eae4fe9034c68bc41d
azdineh <azdineh@gmail.com>
2023-10-01 19:32:18 +0100

    add flags 002

--- public/achamil/flutter_service_worker.js
+++ public/achamil/flutter_service_worker.js
@@ -60,9 +60,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "4c99283b578ef6df0403cef06bcf24d7
 "icons/Icon-512.png": "101dcc66ebdad9efe21cf99f9381dbaf",
 "icons/Icon-maskable-192.png": "d00a6639efc405656cba77c89bc4b0c2",
 "icons/Icon-maskable-512.png": "101dcc66ebdad9efe21cf99f9381dbaf",
-"index.html": "79b9419606068ca4f56c18a1aaff66ee",
-"/": "79b9419606068ca4f56c18a1aaff66ee",
-"main.dart.js": "d63d0f8221d56c30c4e1ca5268db9017",
+"index.html": "5279d0579d36edc1b9664bac4de4716e",
+"/": "5279d0579d36edc1b9664bac4de4716e",
+"main.dart.js": "51fbb8614524c875b04fa4ea9579664c",
 "manifest.json": "d41d8cd98f00b204e9800998ecf8427e",
 "version.json": "f7038de1e3ece356a36ee500f5dbc5c1"};
 // The application shell files that are downloaded before a service worker can
